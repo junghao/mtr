162add1d1a3545834872bce76c4383a485849a9d
Geoff Clitheroe <g.clitheroe@gns.cri.nz>
2016-03-09 21:32:20 +1300

    Increase db connection pool.

--- mtr-api/server.go
+++ mtr-api/server.go
@@ -48,8 +48,8 @@ func main() {
 	}
 	defer db.Close()
 
-	db.SetMaxIdleConns(10)
-	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(30)
+	db.SetMaxOpenConns(30)
 
 	if err = db.Ping(); err != nil {
 		log.Println("ERROR: problem pinging DB - is it up and contactable? 500s will be served")
